620d4c9907b8885575f6e77121563441d7a83023
somebodil <somebodil@naver.com>
2022-04-20 02:12:39 +0900

    Update All

--- ml-bert-sst2-classification/main.py
+++ ml-bert-sst2-classification/main.py
@@ -1,61 +1,66 @@
 import numpy as np
-import pandas as pd
+from datasets import load_dataset
 import torch
 from torch import nn
 from torch.optim import Adam
-from torch.utils.data import DataLoader, Dataset
-from transformers import BertTokenizer, BertForSequenceClassification, BertModel, BertConfig
-
-
-class GlueSst2Dataset(Dataset):
-    def __init__(self, tokenizer, data, max_length):
-        self.labels = [label for label in data['label']]
-
-        self.sentences = [
-            tokenizer(sentence, padding='max_length', max_length=max_length, truncation=True, return_tensors="pt")
-            for sentence in data['sentence']
-        ]
-
-    def classes(self):
-        return self.labels
-
-    def __len__(self):
-        return len(self.labels)
-
-    def __getitem__(self, idx):
-        return self.texts[idx], self.labels[idx]
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from transformers import BertTokenizer, BertModel, BertConfig
 
 
 class BertClassifier(nn.Module):
-    def __init__(self, hidden_size, num_hidden_layers, num_attention_heads, max_length, num_classes, is_cased):
+    def __init__(self, hidden_size, num_hidden_layers, num_attention_heads, num_classes, is_cased):
         super(BertClassifier, self).__init__()
-
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         bert_model_name = 'bert-base-cased' if is_cased else 'bert-base-uncased'
         bert_config = BertConfig(hidden_size=hidden_size,
                                  num_hidden_layers=num_hidden_layers,
                                  num_attention_heads=num_attention_heads)
-        self.model = BertModel(bert_config).from_pretrained(bert_model_name).to(device)
-        self.linear = nn.Linear(hidden_size * max_length, num_classes).to(device)
+        self.model = BertModel(bert_config).from_pretrained(bert_model_name)
+        self.linear = nn.Linear(in_features=hidden_size, out_features=num_classes)
 
     def forward(self, input_ids, attention_mask):
-        model_out = self.model(input_ids=input_ids, attention_mask=attention_mask, return_dict=False)
-        return None
+        _, x = self.model(input_ids=input_ids, attention_mask=attention_mask, return_dict=False)
+        x = self.linear(x)
+        return x
 
 
-def train(dataloader, model, loss_fn, optimizer):
-    model.train()
+def train(device, dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train().to(device)
+    loss_fn.to(device)
+
+    for i, batch in enumerate(tqdm(dataloader)):
+        batch = {k: v.to(device) for k, v in batch.items()}
+
+        # Compute prediction error
+        predict = model(batch['input_ids'], batch['attention_mask'])
+        loss = loss_fn(predict, batch['labels'])
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if i % 10 == 0:
+            loss, current = loss.item(), i * dataloader.batch_size
+            print(f"Train loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+def encode(examples, tokenizer, max_length):
+    encoded = tokenizer(examples['sentence'], max_length=max_length, truncation=True, padding='max_length')
+    return encoded
 
 
 def main():
     # Device --
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    # device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = "cpu"
 
     # Hyper parameter --
     np.random.seed(4885)
     learning_rate = 1e-3
-    batch_size = 64
+    batch_size = 16
     epochs = 5
     max_length = 512
     hidden_size = 768
@@ -63,20 +68,23 @@ def main():
     num_attention_heads = 12
 
     # Dataset --
-    training_data = pd.read_csv('./data/glue_data/SST-2/train.tsv', delimiter='\t')
-    test_data = pd.read_csv('./data/glue_data/SST-2/test.tsv', delimiter='\t')
+    train_dataset = load_dataset('glue', 'sst2', split="train")
 
     # Prepare tokenizer, dataloader, model, loss function, optimizer, etc --
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-    train_dataloader = DataLoader(GlueSst2Dataset(tokenizer, training_data, max_length), batch_size=batch_size)
-    test_dataloader = DataLoader(GlueSst2Dataset(tokenizer, test_data, max_length), batch_size=batch_size)
 
-    model = BertForSequenceClassification.from_pretrained("bert-base-uncased").to(device)
+    train_dataset = train_dataset.map(lambda examples: encode(examples, tokenizer, max_length), batched=True)
+    train_dataset = train_dataset.map(lambda examples: {'labels': examples['label']}, batched=True)
+    train_dataset.set_format(type='torch', columns=['input_ids', 'token_type_ids', 'attention_mask', 'labels'])
+
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
+
+    model = BertClassifier(hidden_size, num_hidden_layers, num_attention_heads, np.unique(train_dataset['label']).shape[0], True)
     loss_fn = nn.CrossEntropyLoss()
     optimizer = Adam(model.parameters(), lr=learning_rate)
 
     for t in range(epochs):
-        train(train_dataloader, model, loss_fn, optimizer, device)
+        train(device, train_dataloader, model, loss_fn, optimizer)
 
 
 if __name__ == '__main__':
--- ml-bert-sst2-classification/test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-new_dictionary = {"Micheal": 18, "Elon": 17}
-new_lis = list(new_dictionary.items())
-con_arr = np.array(new_lis)
-print("Convert dict to arr:", con_arr)
