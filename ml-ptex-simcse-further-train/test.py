45101b9f8feda11d1125a48153496682acb035ec
somebodil <somebodil@naver.com>
2022-06-15 23:41:03 +0900

    Update All

---
+++ ml-ptex-simcse-further-train/test.py
@@ -0,0 +1,13 @@
+import torch
+
+# from simcse import SimCSE
+
+# model = SimCSE("princeton-nlp/sup-simcse-bert-base-uncased")
+
+# # encoding sentences into embeddings
+# embeddings = model.encode(['A woman is reading.', 'A man is playing a guitar.'])
+
+# # compute the cosine similarities between two groups of sentences
+# sentences_a = ['A woman is reading.', 'A man is playing a guitar.']
+# sentences_b = ['He plays guitar.', 'A woman is making a photo.']
+# similarities = model.similarity(sentences_a, sentences_b)
