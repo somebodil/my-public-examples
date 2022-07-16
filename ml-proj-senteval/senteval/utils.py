c07285369cecad89cdeae20d27a7154449b59b90
somebodil <somebodil@naver.com>
2022-07-16 13:10:42 +0900

    Commented code because of project known issue

--- ml-proj-senteval/senteval/utils.py
+++ ml-proj-senteval/senteval/utils.py
@@ -85,11 +85,11 @@ def get_optimizer(s):
     else:
         raise Exception('Unknown optimization method: "%s"' % method)
 
-    # check that we give good parameters to the optimizer
-    expected_args = inspect.getargspec(optim_fn.__init__)[0]
-    assert expected_args[:2] == ['self', 'params']
-    if not all(k in expected_args[2:] for k in optim_params.keys()):
-        raise Exception('Unexpected parameters: expected "%s", got "%s"' % (
-            str(expected_args[2:]), str(optim_params.keys())))
+    # check that we give good parameters to the optimizer <= commented safely based on https://github.com/facebookresearch/SentEval/issues/88
+    # expected_args = inspect.getargspec(optim_fn.__init__)[0]
+    # assert expected_args[:2] == ['self', 'params']
+    # if not all(k in expected_args[2:] for k in optim_params.keys()):
+    #     raise Exception('Unexpected parameters: expected "%s", got "%s"' % (
+    #         str(expected_args[2:]), str(optim_params.keys())))
 
     return optim_fn, optim_params
